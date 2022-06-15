7f759e6091a94d8c4a5270d2681b36b625969b93
Bruna <70817817+brunacaveion@users.noreply.github.com>
2022-06-14 21:11:22 -0300

    Add files via upload

---
+++ CleanCode.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+
+	std::cout
+		<< "I" << std::endl
+		<< "AM" << std::endl
+		<< "LEARNING" << std::endl
+		<< "TO" << std::endl
+		<< "PROGRAM" << std::endl
+		<< "WITH" << std::endl
+		<< "C++"
+		<< std::endl;
+	system("PAUSE");
+	return 0;
+
+}
