f533aedf8962c02867df0f5d41b9215b864491ca
Bruna <70817817+brunacaveion@users.noreply.github.com>
2022-06-14 21:00:25 -0300

    Add files via upload

---
+++ HelloWorldAndShortCuts.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+// #include is used for include headline
+//iostream attaches a code into my code
+
+int main()
+/* it is a mais function, all C++ code starts with that*/
+
+{ //starts a block
+
+	// cout stands for output of the code, in this case it will ask to open the prompt
+	std::cout << "Hello C++ :D" << std::endl;
+	//endl stands for break the line
+	//We always use "" for strings
+
+	system("color 5F");
+	// system("color 5F") return purple prompt with white words
+	system("PAUSE");
+	//gives a pause to the prompt
+	
+
+	return 0;
+	// 0 stands for "everything went right"
+
+} //end of a block
+
+// Short cuts
+// ctrl + shift +B compiles the code
+// CTRL + F5 executes the program
+// alt + shift + enter maximize/minimize the window
+//ctrl + K + C add comentars to the code
+// ctrl +C copy the all line without selecting it
