e508a97001f4b0807c4ae21da11a690989eb3f80
Bruna <70817817+brunacaveion@users.noreply.github.com>
2022-06-14 21:13:06 -0300

    Add files via upload

---
+++ EasyNewLine.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	// \n é new line
+	std::cout
+		<< "VOU" <<"\n"
+		<< "APRENDER" << "\n"
+		<< "A" << "\n"
+		<< "PROGRAMAR" << "\n"
+		<< "NA" << "\n"
+		<< "LINGUAGEM" << "\n"
+		<< "C++"
+		<< "\n";
+	system("PAUSE");
+	return 0;
+
+}
